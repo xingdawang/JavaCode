ca71681c8a57044447d809d78d3f262b3c3224a0
Xingda Wang <wxd598113636@gmail.com>
2015-01-27 15:28:17 +0000

    This is the hello world edition

--- test/src/test/Solu.java
+++ test/src/test/Solu.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Solu {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String string = scanner.nextLine();
+		//String string = scanner.nextLine();
+		String string = "Hello world!";
 		System.out.println(string);
 	}
 }
