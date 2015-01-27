c250b1a2be3f73e2ba9451aab48351ea9c966716
Xingda Wang <wxd598113636@gmail.com>
2015-01-27 15:30:32 +0000

    This is the you can try edition

--- test/src/test/Solu.java
+++ test/src/test/Solu.java
@@ -6,7 +6,7 @@ public class Solu {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		//String string = scanner.nextLine();
-		String string = "Hello world!";
+		String string = "You can have a try!";
 		System.out.println(string);
 	}
 }
